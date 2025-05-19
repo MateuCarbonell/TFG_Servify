fd19aa8a816996c38c9e69bf67c0331e179ef440
MateuCarbonell <mcarbonell@cifpfbmoll.eu>
2025-05-19 14:55:06 +0200

    Add: add protected profile edit page with user info

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import { getUserFromCookie } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import EditarPerfilForm from "./EditProfileForm";
+
+export default async function Page() {
+  const user = await getUserFromCookie();
+
+  if (!user) redirect("/");
+
+  return <EditarPerfilForm user={user} />;
+}
